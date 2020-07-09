41fa8d5ec022aa4ce35465f4a26744e930f56756
Walter Angolar <walter.silva@tistech.co.ao>
2020-07-09 01:54:05 +0100

    teste sonarcloud coverage

--- Dockerfile
+++ Dockerfile
@@ -7,5 +7,3 @@ WORKDIR /code
 COPY requirements.txt /code
 RUN pip install -r requirements.txt
 COPY . /code/
-
-EXPOSE 8000
--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-
+password = "12346"
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codeprogress.settings')
     try:
